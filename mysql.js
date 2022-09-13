d22a6cb4fed5d540943f296badbb0014f17797d8
LucasMartins <tiprontocardio@gmail.com>
2022-09-13 15:43:24 -0300

    teste

--- mysql.js
+++ mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 var pool = mysql.createPool({
-    "user": "root",
-    "password": "",
-    "database": "prontocardio",
-    "host": "localhost",
+    "user": "bf9edaa381acdb",
+    "password": "21829e51",
+    "database": "heroku_1c35cff35132432",
+    "host": "us-cdbr-east-06.cleardb.net",
     "port": 3306
 })
 
